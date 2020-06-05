c4993708f7d5d7c31c684cbdcf01aa8dd084a619
Hryhorii <h.bandurian@gmail.com>
2020-06-05 23:00:57 +0300

    Optional effects base for consumable items

--- src/Collectibles/Collectable.gd
+++ src/Collectibles/Collectable.gd
@@ -3,5 +3,7 @@ extends Area2D
 func _on_body_entered(body):
 	if body is Player:
 		Events.emit_signal("item_collected")
+		var effect : ItemEffectBase = get_node("Effect")
+		effect.apply_effect()
 		queue_free()
 	pass 
