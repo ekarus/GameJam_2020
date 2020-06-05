2d4a1353841f254537cbd986a531a3bfc102938e
Pavel Grebnev <gamedevraccoon@gmail.com>
2020-06-05 21:18:58 +0300

    Code for horisontal action selection panel

--- src/UI/MoveSelection/MoveSelectionPanel.gd
+++ src/UI/MoveSelection/MoveSelectionPanel.gd
@@ -14,11 +14,12 @@ var active_variants = []
 
 var start_speed = 100.0
 var speed = start_speed
-var variant_spawn_height = 20.0
-var variant_collision_height = 10.0
+var variant_spawn_width = 50.0
+var variant_collision_width = 10.0
 
 var scene_center
 var scene_half_height
+var scene_half_width
 var item_collision_half_size
 
 # for adjusting priority of subsequent variants
@@ -35,9 +36,10 @@ func _ready():
 	add_child(spawn_timer)
 	
 	scene_center = $PanelArea/CollisionShape2D.position
+	scene_half_width = ($PanelArea/CollisionShape2D.shape as RectangleShape2D).extents.x
 	scene_half_height = ($PanelArea/CollisionShape2D.shape as RectangleShape2D).extents.y
 	
-	item_collision_half_size = ($Cursor/CollisionShape2D.shape as RectangleShape2D).extents.y + variant_collision_height/2
+	item_collision_half_size = ($Cursor/CollisionShape2D.shape as RectangleShape2D).extents.x + variant_collision_width/2
 	
 	_spawn_new_variant()
 
@@ -58,14 +60,14 @@ func _process(delta):
 func _spawn_new_variant():
 	# restart timer
 	# ToDo: better randomization that takes speed into account
-	spawn_timer.start(rng.randf_range(variant_spawn_height / speed, 1))
+	spawn_timer.start(rng.randf_range(variant_spawn_width / speed, 1))
 	
 	# spawn
 	var new_scene = variant_scenes[rng.randi_range(0, variant_scenes.size() - 1)]
 	
 	var node_instance = new_scene.instance()
 	$VariantsContainer.add_child(node_instance)
-	node_instance.position = Vector2(scene_center.x, scene_center.y - scene_half_height)
+	node_instance.position = Vector2(scene_center.x - scene_half_width, scene_center.y)
 	active_variants.append(node_instance)
 
 
@@ -80,7 +82,7 @@ func _despawn_old_variants():
 	var variants_to_despawn = []
 	
 	for variant in active_variants:
-		if variant.position.y > scene_center.y + scene_half_height + variant_spawn_height:
+		if variant.position.x > scene_center.x + scene_half_width + variant_spawn_width:
 			variants_to_despawn.append(variant)
 	
 	_despawn_variant_items(variants_to_despawn)
@@ -88,7 +90,7 @@ func _despawn_old_variants():
 
 func _update_positions(delta):
 	for variant in active_variants:
-		variant.position.y += speed * delta
+		variant.position.x += speed * delta
 
 
 func _select_hovered_action():
@@ -96,10 +98,10 @@ func _select_hovered_action():
 	var min_diff = item_collision_half_size
 	
 	for variant in active_variants:
-		var pos_diff = abs(variant.get_global_position().y - $Cursor/CollisionShape2D.get_global_position().y + variant_center_shift)
+		var pos_diff = abs(variant.get_global_position().x - $Cursor/CollisionShape2D.get_global_position().x + variant_center_shift)
 		if pos_diff < min_diff:
 			variant_to_activate = variant
-
+	
 	if variant_to_activate != null:
 		var variant_base = variant_to_activate.get_node("VariantBase")
 		_activate_action(variant_base.variant_type, variant_base.steps_count)
