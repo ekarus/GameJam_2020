2f2b3a20d5ee288328d7ad1fb5744fef1617a1da
Hryhorii <h.bandurian@gmail.com>
2020-06-05 14:18:09 +0300

    Wireframe for Global events
    - To emit signal use Events.emit_signal()
    - To subscribe for signal use Events.connect

---
+++ src/Autoload/Events.gd
@@ -0,0 +1,11 @@
+# Autoloaded singleton that holds signals that would be troublesome to wire in a
+# local parent or a scene owner.
+# 
+# This keeps objects passed through setup functions or unsafe accessors at a
+# lower count, and can be replaced with simpler `Events.connect` calls.
+extends Node
+
+signal player_died
+signal quit_requested
+
+signal player_action_choosen
