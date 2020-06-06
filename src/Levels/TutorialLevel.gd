227a0484c04c295ecb37f49f75d97dfce7d3160c
Ruslan <ekarus303@gmail.com>
2020-06-06 19:30:35 +0300

    Crash fix

--- src/Levels/TutorialLevel.gd
+++ src/Levels/TutorialLevel.gd
@@ -3,7 +3,7 @@ extends GameLevel
 
 func _ready():
 	Events.connect("start_game", self, "start_new_game")
-	if GameFlow.main_menu_instance.visible:
+	if GameFlow.main_menu_instance != null and GameFlow.main_menu_instance.visible:
 		GameFlow.hide_hud()
 		GameFlow.lock_character_input()
 
