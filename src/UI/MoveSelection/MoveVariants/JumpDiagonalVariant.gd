76c76dd3ebb7f9ad0c5cab011fc7bfadfb9bc19e
Pavel Grebnev <gamedevraccoon@gmail.com>
2020-06-06 10:33:34 +0300

    Fix crash without label on move variants

--- src/UI/MoveSelection/MoveVariants/JumpDiagonalVariant.gd
+++ src/UI/MoveSelection/MoveVariants/JumpDiagonalVariant.gd
@@ -4,5 +4,5 @@ extends Node2D
 func _ready():
 	var count = randi() % 4 + 1
 	$VariantBase.steps_count = count
-	if count > 0:
+	if count > 1 and $Label != null:
 		$Label.text = $Label.text + (" x%s" % count)
